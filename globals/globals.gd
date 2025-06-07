9f935fbb239bf8cfdb0b0be9af1c417c1978523d
Arjun <62688208+asterbot@users.noreply.github.com>
2025-06-07 15:31:37 -0400

    Enemies (#5)
    * wave mechanics
    
    * background + enemies spawning + waves + COLORS

--- globals/globals.gd
+++ globals/globals.gd
@@ -1,3 +1,12 @@
 extends Node
 
-var NUM_Z_INDICES = 10
+var NUM_Z_INDICES = 100
+
+var cur_wave = 0
+
+var WAVES = {
+	0: {
+		"cats": 4,
+		"bushes": 4
+	}
+}
--- scenes/cat_enemies/test_cat/test_cat.gd
+++
@@ -1,8 +0,0 @@
-extends Cat
-
-
-func _ready() -> void:
-	super()
-	
-func _process(_delta: float) -> void:
-	super(_delta)
