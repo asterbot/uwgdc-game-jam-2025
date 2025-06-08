560ba92fc8929a68250eeed8d9ec9aa652ddb374
asterbot <arjunsodhi7@gmail.com>
2025-06-08 15:42:26 -0400

    win text

--- globals/globals.gd
+++ globals/globals.gd
@@ -13,36 +13,35 @@ var score = 0
 
 var WAVES = {
 	0: {
-		"ground_cats": 3,
-		"balloon_cats": 2,
-		"bushes": 2,
-		"spawn_cooldown": 0.5
+		"ground_cats": 4,
+		"balloon_cats": 0,
+		"bushes": 0,
+		"spawn_cooldown": 3
 	},
 	1: {
-		"ground_cats": 30,
+		"ground_cats": 4,
 		"balloon_cats": 0,
-		"bushes": 10,
-		"spawn_cooldown": 0.5
+		"bushes": 6,
+		"spawn_cooldown": 2.5
 	},
 	2: {
-		"ground_cats": 30,
-		"balloon_cats": 0,
-		"bushes": 10,
-		"spawn_cooldown": 0.5
+		"ground_cats": 8,
+		"balloon_cats": 2,
+		"bushes": 8,
+		"spawn_cooldown": 2
 	},
 	3: {
-		"ground_cats": 30,
-		"balloon_cats": 0,
-		"bushes": 10,
-		"spawn_cooldown": 0.5
+		"ground_cats": 20,
+		"balloon_cats": 6,
+		"bushes": 9,
+		"spawn_cooldown": 1.5
 	},
 	4: {
 		"ground_cats": 30,
-		"balloon_cats": 0,
+		"balloon_cats": 10,
 		"bushes": 10,
-		"spawn_cooldown": 0.5
+		"spawn_cooldown": 1
 	}
-	
 }
 
 var NUM_WAVES = WAVES.size()
