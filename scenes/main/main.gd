d03539ac2f679fc74ae8c5381d9bec1aa7697bcd
asterbot <arjunsodhi7@gmail.com>
2025-06-08 15:00:56 -0400

    balloons + fall mechanic + score + music asset from invertedcarrot

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -6,14 +6,15 @@ extends Node2D
 
 var bush_scene: PackedScene = preload("res://scenes/bush/bush.tscn")
 var cat_scene: PackedScene = preload("res://scenes/cat_enemies/cat.tscn")
+var balloon_scene: PackedScene = preload("res://scenes/cat_enemies/balloon.tscn")
 
 var viewport_size: Vector2
 
 var can_spawn = true
 var enemy_queue: Array = []
 
-
-func spawn_entities_random(entity_scene: PackedScene, folder: Node2D ,n: int, off_screen: bool = false, use_timer: bool = false):
+func spawn_entities_random(entity_scene: PackedScene, folder: Node2D ,n: int, 
+				off_screen: bool = false, use_timer: bool = false, with_balloon: bool = false):
 	"""
 	Spawn n random instances of entity_scene in folder of main
 	Spawns entity off screen if off_screen is true
@@ -27,7 +28,10 @@ func spawn_entities_random(entity_scene: PackedScene, folder: Node2D ,n: int, of
 		if (! off_screen): position_x = randf_range(100, width-100)
 		else: position_x = [-70, viewport_size.x + 70].pick_random()
 		
-		var position_y = randf_range(0.43*height, height)
+		var position_y = 0
+		if (!with_balloon): position_y = randf_range(0.43*height, height)
+		else: position_y = randf_range(0.1*height, 0.4*height) # in the air
+		
 		positions.append(Vector2(position_x, position_y))
 	
 	for i in range(n):
@@ -35,14 +39,30 @@ func spawn_entities_random(entity_scene: PackedScene, folder: Node2D ,n: int, of
 		entity.position = positions[i]
 		
 		# Add the entity to the queue for spawning
-		if (use_timer) : enemy_queue.append(entity)
-		else: folder.add_child(entity)
+		if (use_timer) : 
+			enemy_queue.append(entity)
+			if (with_balloon):
+				var balloon = balloon_scene.instantiate()
+				entity.has_balloon = true
+				entity.balloon = balloon
+				balloon.entity_to_follow = entity
+		else: 
+			folder.add_child(entity)
+			if (with_balloon):
+				var balloon = balloon_scene.instantiate()
+				entity.has_balloon = true
+				entity.balloon = balloon
+				balloon.entity_to_follow = entity
+				$Balloons.add_child(balloon)
+				
+
 
 func new_wave():
 	"""Call this when a wave change is needed"""
 	Globals.cur_wave += 1
 	var num_bushes = Globals.WAVES[Globals.cur_wave]["bushes"]
-	var num_cats = Globals.WAVES[Globals.cur_wave]["cats"]
+	var num_ground_cats = Globals.WAVES[Globals.cur_wave]["ground_cats"]
+	var num_balloon_cats = Globals.WAVES[Globals.cur_wave]["balloon_cats"]
 	var spawn_cooldown = Globals.WAVES[Globals.cur_wave]["spawn_cooldown"]
 	
 
@@ -50,7 +70,10 @@ func new_wave():
 	$SpawnTimer.start()
 	
 	spawn_entities_random(bush_scene, $Bushes, num_bushes)
-	spawn_entities_random(cat_scene, $Enemies, num_cats, true, true)
+	spawn_entities_random(cat_scene, $Enemies, num_ground_cats, true, true)
+	spawn_entities_random(cat_scene, $Enemies, num_balloon_cats, true, true, true)
+	
+	enemy_queue.shuffle()
 
 
 func _ready() -> void:
@@ -65,6 +88,9 @@ func _process(delta: float) -> void:
 	if (!enemy_queue.is_empty() and can_spawn):
 		var enemy = enemy_queue.pop_front()
 		$Enemies.add_child(enemy)
+		if (enemy.has_balloon):
+			var balloon = enemy.balloon
+			$Balloons.add_child(balloon)
 		can_spawn = false
 
 	if ($Enemies.get_child_count()==0 and enemy_queue.is_empty()):
@@ -83,6 +109,7 @@ func _process(delta: float) -> void:
 func _on_projectiles_child_entered_tree(projectile: CharacterBody2D) -> void:
 	# when projectile enters this folder, connect its signal to it
 	projectile.connect("notify_cat_hit", score_node.increment_score)
+	projectile.connect("notify_balloon_hit", score_node.increment_score)
 
 
 func _on_spawn_timer_timeout() -> void:
